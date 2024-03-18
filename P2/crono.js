32ec13db5e4d9ab1a9762f9d22360e4071aafb09
mario <m.fernandezn.2021@alumnos.urjc.es>
2024-03-18 15:16:46 +0100

    Modificaciones P2

--- P2/crono.js
+++ P2/crono.js
@@ -1,140 +1,180 @@
 //-- Clase cronómetro
 class Crono {
 
-  //-- Constructor. Hay que indicar el 
-  //-- display donde mostrar el cronómetro
-  constructor(display) {
-      this.display = display;
-
-      //-- Tiempo
-      this.cent = 0, //-- Centésimas
-      this.seg = 0,  //-- Segundos
-      this.min = 0,  //-- Minutos
-      this.timer = 0;  //-- Temporizador asociado
-  }
-
-  //-- Método que se ejecuta cada centésima
-  tic() {
-      //-- Incrementar en una centesima
-      this.cent += 1;
-
-      //-- 100 centésimas hacen 1 segundo
-      if (this.cent == 100) {
-      this.seg += 1;
-      this.cent = 0;
-      }
-
-      //-- 60 segundos hacen un minuto
-      if (this.seg == 60) {
-      this.min = 1;
-      this.seg = 0;
-      }
-
-      //-- Mostrar el valor actual
-      this.display.innerHTML = this.min + ":" + this.seg + ":" + this.cent
-  }
-
-  //-- Arrancar el cronómetro
-  start() {
-     if (!this.timer) {
-        //-- Lanzar el temporizador para que llame 
-        //-- al método tic cada 10ms (una centésima)
-        this.timer = setInterval( () => {
-            this.tic();
-        }, 10);
-      }
-  }
-
-  //-- Parar el cronómetro
-  stop() {
-      if (this.timer) {
-          clearInterval(this.timer);
-          this.timer = null;
-      }
-  }
-
-  //-- Reset del cronómetro
-  reset() {
-      this.cent = 0;
-      this.seg = 0;
-      this.min = 0;
-
-      this.display.innerHTML = "0:0:0";
-  }
-}
-
-const press = {
-  display : document.getElementById("display"),
-  start : document.getElementById("start"),
-  stop : document.getElementById("stop"),
-  reset : document.getElementById("reset")
-}
-
-console.log("Iniciando ejecución JS...");
-
-const crono = new Crono(press.display);
-
-press.start.onclick = () => {
-  console.log("Start was pressed...");
-  console.log("Generando clave secreta");
-  generarClaveSecreta();
-  crono.start();
-  console.log("Starting crono...");
-}
-
-press.stop.onclick = () => {
-  console.log("Stopping crono...");
-  crono.stop();
-}
-
-let counter = 0;
-
-press.reset.onclick = () => {
-  console.log("Resetting crono...");
-  crono.reset();
-  counter = 0;
-  for (let i = 0; i < 4; i++) {
-    document.getElementById(`clave${counter + 1}`).textContent = '*';
-    counter++;
-  }
-}
-
-
-console.log(press.display.innerHTML);
-
-
-let claveSecreta = [];
-let aciertos = 0;
-
-function generarClaveSecreta() {
-    claveSecreta = [];
-    aciertos = 0;
-    for (let i = 0; i < 4; i++) {
-        claveSecreta.push(Math.floor(Math.random() * 10));
+    constructor(display) {
+        this.display = display;
+  
+        //-- Tiempo
+        this.cent = 0, //-- Centésimas
+        this.seg = 0,  //-- Segundos
+        this.min = 0,  //-- Minutos
+        this.timer = 0;  //-- Temporizador asociado
     }
-    console.log("Clave secreta generada...", claveSecreta);
-}
-
-
-function presionarDigito1(digito) {
-    cont = 0;
-    console.log("Boton con valor pulsado =>", digito);
-    claveSecreta.forEach((valor, indice) => {
-        if(digito == valor) {
-            document.getElementById(`clave${indice + 1}`).textContent = digito;
-            console.log("Valor acertado");
-            cont++;
-            aciertos++;
-        } 
-    })
-    if (cont == 0) {
-        console.log("Valor NO acertado"); // RECORRE TODOS LOS PARAMETROS POR ESTO APARECE EN CONSOLA CUANDO EL VALOR NO SE ENCUENTRA EN ALGUNA DE LAS POSICIONES DE LA CLAVESECRETA
+  
+    //-- Método que se ejecuta cada centésima
+    tic() {
+        //-- Incrementar en una centesima
+        this.cent += 1;
+  
+        //-- 100 centésimas hacen 1 segundo
+        if (this.cent == 100) {
+        this.seg += 1;
+        this.cent = 0;
+        }
+  
+        //-- 60 segundos hacen un minuto
+        if (this.seg == 60) {
+        this.min = 1;
+        this.seg = 0;
+        }
+  
+        //-- Mostrar el valor actual
+        this.display.innerHTML = this.min + ":" + this.seg + ":" + this.cent
     }
-    if (aciertos == 4) {
+  
+    //-- Arrancar el cronómetro
+    start() {
+        if (!this.timer) {
+          //-- Lanzar el temporizador para que llame 
+          //-- al método tic cada 10ms (una centésima)
+          this.timer = setInterval( () => {
+              this.tic();
+          }, 10);
+        }
+    }
+  
+    //-- Parar el cronómetro
+    stop() {
+        if (this.timer) {
+            clearInterval(this.timer);
+            this.timer = null;
+        }
+    }
+  
+    //-- Reset del cronómetro
+    reset() {
+        this.cent = 0;
+        this.seg = 0;
+        this.min = 0;
+  
+        this.display.innerHTML = "0:0:0";
+    }
+  }
+    
+    const press = {
+      display : document.getElementById("display"),
+      start : document.getElementById("start"),
+      stop : document.getElementById("stop"),
+      reset : document.getElementById("reset")
+    }
+    
+    console.log("Starting JS execution...");
+    window.alert("Bienvenido a la práctica 2 (BOOM) Esta consta de 10 botones, un cronómetro y una clave secreta a adivinar. Esta es aleatoria y esta compuesta por 4 dígitos, cuando adivines la clave secreta parará el cronómetro")
+    
+    const crono = new Crono(press.display);
+  
+    let counter = 0;
+    let estadoInicio = false;
+  
+    function resetDisplay() { // Reseteo del display de clave secreta
+      counter = 0;
+      for (let i = 0; i < 4; i++) {
+        document.getElementById(`clave${counter + 1}`).textContent = '*';
+        document.getElementById(`clave${counter + 1}`).style.color = "red";
+        counter++;
+      }
+    }
+  
+    function startGame() {
+      console.log("Start was pressed...");
+      console.log("Generating secret key");
+      resetDisplay();
+      generarClaveSecreta();
+      crono.reset();
+      crono.start();
+      console.log("Starting crono...");
+    }
+  
+    document.addEventListener('keydown', function(event) {  // Verificar si la tecla presionada es numérica (0-9)
+      if (event.key >= '0' && event.key <= '9') {
+        console.log("The key has been pressed: ", event.key);
+        pressedButton = parseInt(event.key);
+        presionarDigito1(pressedButton);
+      }
+    });
+  
+    press.start.onclick = () => {
+      startGame();
+      estadoInicio = true;
+    }
+    
+    press.stop.onclick = () => {
+      console.log("Stopping crono...");
+      crono.stop();
+      estadoInicio = false;
+    }
+    
+    press.reset.onclick = () => {
+      console.log("Resetting crono...");
+      crono.reset();
+      resetDisplay(); 
+    }
+    
+    console.log(press.display.innerHTML);
+    
+    let claveSecreta = [];
+    let aciertos = 0;
+    
+    
+    function generarClaveSecreta() {
+        claveSecreta = [];
+        aciertos = 0;
+        for (let i = 0; i < 4; i++) {
+            claveSecreta.push(Math.floor(Math.random() * 10));
+        }
+        console.log("Secret key generated... ", claveSecreta);
+    }
+    
+    function presionarDigito1(digito) {
+      if (estadoInicio === false) {
+        startGame();
+        estadoInicio = true;
+      }
+      cont = 0;
+      console.log("Button with pressed value => ", digito);
+      claveSecreta.forEach((valor, indice) => {
+          if(digito == valor) {
+              document.getElementById(`clave${indice + 1}`).textContent = digito;
+              document.getElementById(`clave${indice + 1}`).style.color = "rgb(51, 255, 0)";
+              console.log("Successful value");
+              delete claveSecreta[indice]; // Elimino elementos del array para que si se repite la pulsacion sobre un numero no pare el crono
+              cont++;
+              aciertos++;
+          } 
+      })
+      console.log(claveSecreta)
+      if (cont == 0) {
+          console.log("Value NOT successful");
+      }
+      if (aciertos == 4) {
         crono.stop();
+        estadoInicio = false;
+        Swal.fire({
+          title: 'Congratulations!',
+          text: `Your gaming time is: ${document.getElementById("display").textContent} seconds`,
+          icon: 'success',
+          showCancelButton: true,
+          cancelButtonText: 'Close',
+          confirmButtonColor: '#2eb82e',
+          confirmButtonText: 'Reset Game'
+        }).then((result) => {
+          if (result.isConfirmed) {
+            console.log("Resetting crono...");
+            crono.reset();
+            resetDisplay(); 
+          }
+        });
         console.log("Tiempo de juego: ", document.getElementById("display").textContent);
+      }
     }
-}
-
-// AÑADIR VARIABLE DE ESTADO: ON/OFF PARA SABER SI ESTA INICIADO EL CONTADOR, SI NO ESTA INICIADO Y SE PULSA UN BOTON YA SEA NUMERO O START QUE INICIE, SI ESTA INICIADO EL CONTADOR, ESTA FUNCION NO HARA NADA
-// Cambiar linea 126 y 127, cambiar por un else
+  
